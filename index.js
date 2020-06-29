a81b88d5d5a61e89375891ebd02999668ee3ec95
Jocelyn <jocelyn.roul@etu.univ-nantes.fr>
2020-06-30 00:11:09 +0200

    docs : correction de ESLint

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-let app = require('express')()
+const app = require('express')();
 
-require('dotenv').config()
+require('dotenv').config();
 
 const { Client } = require('pg');
 
 const client = new Client({
-    user: process.env.PGUSER,
-    host: process.env.PGHOST,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT,
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
 });
 
 client.connect();
 
 client.query('SELECT NOW()', (err, res) => {
-    console.log(err, res)
-    client.end()
-})
+  console.log(err, res);
+  client.end();
+});
 
 app.get('/', (req, res) => {
-    res.send("Hello World")
-})
+  res.send('Hello World');
+});
 
-app.listen(8080)
+app.listen(8080);
