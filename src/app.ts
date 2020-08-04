9304a74f9ce2310e107c4d37fc2b7c457e52d16d
Jocelyn <jocelyn.roul@etu.univ-nantes.fr>
2020-08-05 01:06:44 +0200

    fix : appel du frontend

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,12 @@ import express from 'express';
 import indexRouter from './router/index';
 import dotenv from 'dotenv';
 import connection from './config/DBConnection';
+import cors from 'cors';
 
 dotenv.config()
 const app = express();
 
+app.use(cors());
 app.use('/', indexRouter);
 
 connection.then(() => {
--- src/controllers/WebtoonsController.ts
+++ src/controllers/WebtoonsController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import webtoonsDAO from '../daos/WebtoonsDAO';
 
 export const getAllWebtoon = (req: Request, res: Response) => {
+    // res.setHeader('Access-Control-Allow-Origin', '*')
     webtoonsDAO.getAllWebtoon((listWebtoon) => {
         res.json(listWebtoon);
     })
